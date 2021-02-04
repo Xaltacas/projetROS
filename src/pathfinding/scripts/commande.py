8a50a97e3f676bff0d7270cb2e25a710e709a072
FERRIER Simon <3805354@pc4087.polytech.upmc.fr>
2021-02-04 15:12:38 +0100

    normalisation angles

--- src/pathfinding/scripts/commande.py
+++ src/pathfinding/scripts/commande.py
@@ -5,13 +5,11 @@ def commande(pos_rob,point_objectif) :
     dx = point_objectif[0] - pos_rob[0] #distance entre le robot et le point projetee sur x
     dy = point_objectif[1] - pos_rob[1] #distance entre le robot et le point projetee sur y
     dist = m.sqrt(dx**2 + dy**2) #distance euclidienne
-    theta = pos_rob[5]  #angle du robot
-    if(theta > m.pi):
-        theta -= 2* m.pi
+    theta = normalize_angle(pos_rob[5])  #angle du robot
     print(theta)
     beta = m.atan2(dy,dx) #angle du point par rapport au robot
     print(beta)
-    alpha = (beta - theta + m.pi)%(2*m.pi) - m.pi #difference d'angle
+    alpha = normalize_angle(beta - theta) #difference d'angle
     print(alpha)
     beta_suivant = point_objectif[3]
     dbeta = beta_suivant-beta
@@ -28,5 +26,12 @@ def commande(pos_rob,point_objectif) :
     return u ,w
     
     
-    
+def normalize_angle(angle) : #Normalise un angle entre -pi et pi
+
+    while(angle > m.pi or angle < -m.pi) :
+        if(angle > m.pi) :
+            angle -= 2*m.pi  
+        elif(angle < -m.pi) :
+            angle  += 2*m.pi 
+    return(angle) 
     
