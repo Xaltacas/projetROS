1f6e03c7358b19381e0cf8e4b55d757ce17577d9
FERRIER Simon <3805354@pc4087.polytech.upmc.fr>
2021-02-04 14:51:16 +0100

    test

--- src/commande.py
+++ src/commande.py
@@ -7,10 +7,12 @@ def commande(pos_rob,point_objectif) :
     dist = m.sqrt(dx**2 + dy**2) #distance euclidienne
     theta = pos_rob[5]  #angle du robot
     beta = m.atan2(dy,dx) #angle du point par rapport au robot
+    print(beta)
     alpha = beta - theta #difference d'angle
+    print(alpha)
     beta_suivant = point_objectif[5]
-    dbeta = beta_suivant-beta
-    
+    dbeta = beta_suivant - beta
+    print(dbeta)
     ## Gains ##
     Kp = 1
     Ka = 1
@@ -22,10 +24,13 @@ def commande(pos_rob,point_objectif) :
     
     
     
-    res = [u,w]
+    return u, w
     
-    return res
     
+pos_rob = [-0.2529,-0.5586,0.02,0,0,87.4663]
+point_objectif = [-0.32,0.8575,0,0,0,89.1113]
+
+print(commande(pos_rob, point_objectif))
     
     
     
